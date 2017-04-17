f1ad58e27cf81fad6c14b511902abeade0e9622e
cliff.neames <cliff.neames@gmail.com>
2017-04-17 15:24:27 -0500

    Refactored into a Method

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -8,48 +8,52 @@ namespace NumberGuesser
 {
 	class Program
 	{
+        static void GuessingGame()
+        {
+            Random randomNumber = new Random();
+
+            int random = randomNumber.Next(1, 101);
+            int guess = 0, counter = 0;
+            string possibleNumber = null;
+
+            do
+            {
+
+                Console.WriteLine("Please guess a number between 1 and 100: ");
+                possibleNumber = Console.ReadLine();
+
+                if (int.TryParse(possibleNumber, out guess) && guess > random)
+                {
+                    Console.WriteLine("The number is lower than what you guessed");
+                    counter++;
+                }
+                else if (int.TryParse(possibleNumber, out guess) && guess < random)
+                {
+                    Console.WriteLine("The number is higher than what you guessed");
+                    counter++;
+                }
+                else if (int.TryParse(possibleNumber, out guess) && guess == random)
+                {
+                    Console.WriteLine("You've matched the number exactly you WIN!!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Your guess was not valid");
+                }
+
+                if (counter == 5)
+                {
+                    Console.WriteLine("You Lose Sucker!");
+                }
+            }
+            while (counter < 5);
+        }
 		static void Main(string[] args)
 		{
 
-			Random randomNumber = new Random();
-
-			int random = randomNumber.Next(1, 101);
-			int guess = 0, counter = 0;
-			string possibleNumber = null;
-
-			do
-			{
-
-				Console.WriteLine("Please guess a number between 1 and 100: ");
-				possibleNumber = Console.ReadLine();
-
-				if (int.TryParse(possibleNumber, out guess) && guess > random)
-				{
-					Console.WriteLine("The number is lower than what you guessed");
-					counter++;
-				}
-				else if (int.TryParse(possibleNumber, out guess) && guess < random)
-				{
-					Console.WriteLine("The number is higher than what you guessed");
-					counter++;
-				}
-				else if (int.TryParse(possibleNumber, out guess) && guess == random)
-				{
-					Console.WriteLine("You've matched the number exactly you WIN!!");
-					break;
-				}
-				else
-				{
-					Console.WriteLine("Your guess was not valid");
-				}
-
-				if (counter == 5)
-				{
-					Console.WriteLine("You Lose Sucker!");
-				}
-			}
-			while (counter < 5);
-					
+            GuessingGame();
+            
 		}
 	}
 }
